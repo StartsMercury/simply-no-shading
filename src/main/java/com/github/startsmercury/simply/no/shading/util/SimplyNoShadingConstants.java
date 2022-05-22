0333232b1f678297579ea2e8f5c755e58fbedfee
StartsMercury <startsmercury@gmail.com>
2022-05-22 15:00:02 +0800

    Removed Deprecated Code

--- src/main/java/com/github/startsmercury/simply/no/shading/util/SimplyNoShadingConstants.java
+++ src/main/java/com/github/startsmercury/simply/no/shading/util/SimplyNoShadingConstants.java
@@ -4,26 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public final class SimplyNoShadingConstants {
-	@Deprecated(forRemoval = true)
-	public static final boolean FABRIC;
-
-	public static final Gson GSON;
-
-	static {
-		FABRIC = isClassLoaded("net.fabricmc.loader.api.FabricLoader");
-		GSON = new GsonBuilder().setPrettyPrinting().create();
-	}
-
-	@Deprecated(forRemoval = true)
-	private static boolean isClassLoaded(final String className) {
-		try {
-			Class.forName(className);
-
-			return true;
-		} catch (final ClassNotFoundException cnfe) {
-			return false;
-		}
-	}
+	public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
 	private SimplyNoShadingConstants() {
 	}
