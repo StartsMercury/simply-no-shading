9483a6d1c8c65918e44306144a3b9dd94aea4dc9
StartsMercury <startsmercury@gmail.com>
2022-06-01 15:19:48 +0800

    Document `impl` subpackage

--- src/main/java/com/github/startsmercury/simply/no/shading/impl/CloudRenderer.java
+++ src/main/java/com/github/startsmercury/simply/no/shading/impl/CloudRenderer.java
@@ -2,9 +2,20 @@ package com.github.startsmercury.simply.no.shading.impl;
 
 import static net.fabricmc.api.EnvType.CLIENT;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import net.fabricmc.api.Environment;
 
+/**
+ * Represents a cloud renderer.
+ *
+ * @since 5.0.0
+ */
+@ApiStatus.Internal
 @Environment(CLIENT)
 public interface CloudRenderer {
+	/**
+	 * Generate the clouds.
+	 */
 	void generateClouds();
 }
--- src/main/java/com/github/startsmercury/simply/no/shading/impl/Shadable.java
+++ src/main/java/com/github/startsmercury/simply/no/shading/impl/Shadable.java
@@ -2,15 +2,35 @@ package com.github.startsmercury.simply.no.shading.impl;
 
 import static net.fabricmc.api.EnvType.CLIENT;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import com.github.startsmercury.simply.no.shading.config.ShadingRule;
 
 import net.fabricmc.api.Environment;
 
+/**
+ * Represents a object that shading can be applied bounded to a
+ * {@link ShadingRule}.
+ *
+ * @since 5.0.0
+ */
+@ApiStatus.Internal
 @Environment(CLIENT)
 public interface Shadable {
+	/**
+	 * @return the current shading rule
+	 */
 	ShadingRule getShadingRule();
 
+	/**
+	 * @return the shade with the shading rule applied
+	 */
 	boolean isShade();
 
+	/**
+	 * Sets the current shading rule to a new one.
+	 *
+	 * @param shadingRule the new shading rule
+	 */
 	void setShadingRule(ShadingRule shadingRule);
 }
