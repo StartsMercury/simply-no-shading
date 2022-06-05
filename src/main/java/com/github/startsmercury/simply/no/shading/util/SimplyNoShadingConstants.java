400d1c05f059c174b3ec737d34f13d5bbbc1fd40
StartsMercury <startsmercury@gmail.com>
2022-06-05 22:00:55 +0800

    Updated Documentation for The Rest of The Util Subpackage

--- src/main/java/com/github/startsmercury/simply/no/shading/util/SimplyNoShadingConstants.java
+++ src/main/java/com/github/startsmercury/simply/no/shading/util/SimplyNoShadingConstants.java
@@ -4,20 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
- * Holds constants used throughout elsewhere.
+ * Holds constants used elsewhere by the mod Simply No Shading.
  *
  * @since 5.0.0
  */
 public final class SimplyNoShadingConstants {
 	/**
-	 * The {@link Gson gson}.
+	 * The {@link Gson gson} used in serializing and deserializing json files.
 	 *
 	 * @since 5.0.0
 	 */
 	public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
 	/**
-	 * It is not allowed to instantiate {@code SimplyNoShadingConstants}.
+	 * It is forbidden to create a new {@code SimplyNoShadingConstants}.
 	 */
 	private SimplyNoShadingConstants() {
 	}
--- src/main/java/com/github/startsmercury/simply/no/shading/util/package-info.java
+++ src/main/java/com/github/startsmercury/simply/no/shading/util/package-info.java
@@ -1,6 +1,10 @@
 /**
- * Contains utility classes which maybe subject to breaking changes for
- * dependent classes.
+ * Contains utility classes specific for the mod Simply No Shading. Expect
+ * changes that are unlikely to be backwards compatible here.
+ * <p>
+ * The possibility of some classes being worth published as a separate library
+ * is considered, though the specifics for which and when is not something that
+ * can be answered immediately.
  */
 @Internal
 package com.github.startsmercury.simply.no.shading.util;
