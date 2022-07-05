3e91de4f08b791362db81b2324ab1cd6e4e348db
StartsMercury <startsmercury@gmail.com>
2022-07-05 11:44:18 +0800

    Revert "Updated Mixin Logging Code"
    This reverts commit 1fa20f6246cfec1122ecce3dd05197f785d3587d.

--- src/main/java/com/github/startsmercury/simply/no/shading/mixin/SimplyNoShadingMixinPlugin.java
+++ src/main/java/com/github/startsmercury/simply/no/shading/mixin/SimplyNoShadingMixinPlugin.java
@@ -1,6 +1,5 @@
 package com.github.startsmercury.simply.no.shading.mixin;
 
-import static com.github.startsmercury.simply.no.shading.entrypoint.SimplyNoShadingClientMod.LOGGER;
 import static com.github.startsmercury.simply.no.shading.util.SimplyNoShadingConstants.GSON;
 
 import java.io.IOException;
@@ -11,6 +10,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.objectweb.asm.tree.ClassNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
@@ -33,6 +34,11 @@ public class SimplyNoShadingMixinPlugin implements IMixinConfigPlugin {
 	public static final Path CONFIG_PATH = FabricLoader.getInstance()
 	        .getConfigDir()
 	        .resolve("simply-no-shading+mixin.json");
+
+	/**
+	 * The logger.
+	 */
+	public static final Logger LOGGER = LoggerFactory.getLogger("simply-no-shading/mixin");
 
 	/**
 	 * Creates the mixin config in disk.
