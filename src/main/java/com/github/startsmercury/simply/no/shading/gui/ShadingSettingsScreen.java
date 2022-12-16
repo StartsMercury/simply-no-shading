60e733ab4581f62b34c5e4d7bff0e9833534a000
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2022-12-16 22:17:05 +0800

    Update ShadingSettingsScreen.java
     - 1.19.3 hid native Button constructor, a Builder is used instead.

--- src/main/java/com/github/startsmercury/simply/no/shading/gui/ShadingSettingsScreen.java
+++ src/main/java/com/github/startsmercury/simply/no/shading/gui/ShadingSettingsScreen.java
@@ -133,8 +133,10 @@ public class ShadingSettingsScreen extends OptionsSubScreen {
 		addOptions();
 
 		addWidget(this.list);
-		addRenderableWidget(new Button(this.width / 2 - 100, this.height - 27, 200, 20, CommonComponents.GUI_DONE,
-		        button -> this.minecraft.setScreen(this.lastScreen), null));
+		addRenderableWidget(Button.builder(CommonComponents.DONE, button -> this.minecraft.setScreen(this.lastScreen))
+		        .bounds(this.width / 2 - 100, this.height - 27, 200, 20).build());
+		// addRenderableWidget(new Button(this.width / 2 - 100, this.height - 27, 200, 20, CommonComponents.GUI_DONE,
+		//        button -> this.minecraft.setScreen(this.lastScreen), null));
 
 		LOGGER.debug("Initialized settings screen");
 	}
