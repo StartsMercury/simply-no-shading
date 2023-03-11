5c4b4685ea7e4634bd7bb76064f25d47fe9c4956
StartsMercury <startsmercury@gmail.com>
2023-03-11 15:43:00 +0800

    Fixed mixin target
     - Woah, it was a `boolean` wasn't it?

--- src/client/java/com/github/startsmercury/simply/no/shading/mixin/shading/block/minecraft/LiquidBlockRendererMixin.java
+++ src/client/java/com/github/startsmercury/simply/no/shading/mixin/shading/block/minecraft/LiquidBlockRendererMixin.java
@@ -46,7 +46,7 @@ public class LiquidBlockRendererMixin {
 	 *         {@link Config#blockShadingEnabled block shading is enabled};
 	 *         {@code false} otherwise
 	 */
-	@ModifyArg(method = "tesselate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/material/FluidState;)V",
+	@ModifyArg(method = "tesselate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/material/FluidState;)Z",
 	           at = @At(value = "INVOKE",
 	                    target = "Lnet/minecraft/world/level/BlockAndTintGetter;getShade(Lnet/minecraft/core/Direction;Z)F"),
 	           index = 1)
