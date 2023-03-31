1dc95ebf08cf2c9d6e7ff4445b3b6162e92006c7
StartsMercury <startsmercury@gmail.com>
2023-03-31 17:11:12 +0800

    1.19.2

--- src/client/java/com/github/startsmercury/simply/no/shading/mixin/shading/block/sodium/FluidRendererMixin.java
+++ src/client/java/com/github/startsmercury/simply/no/shading/mixin/shading/block/sodium/FluidRendererMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import com.github.startsmercury.simply.no.shading.client.Config;
 import com.github.startsmercury.simply.no.shading.client.SimplyNoShading;
 
-import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.FluidRenderer;
+import me.jellysquid.mods.sodium.client.render.pipeline.FluidRenderer;
 
 /**
  * The {@code FluidRendererMixin} is a {@linkplain Mixin mixin} class for the
@@ -37,7 +37,7 @@ public class FluidRendererMixin {
 	 *         {@linkplain Config#blockShadingEnabled block shading is enabled};
 	 *         {@code 1.0f} otherwise
 	 */
-	@ModifyVariable(method = "updateQuad(Lme/jellysquid/mods/sodium/client/model/quad/ModelQuadView;Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/core/BlockPos;Lme/jellysquid/mods/sodium/client/model/light/LightPipeline;Lnet/minecraft/core/Direction;FLme/jellysquid/mods/sodium/client/model/quad/blender/ColorSampler;Lnet/minecraft/world/level/material/FluidState;)V",
+	@ModifyVariable(method = "calculateQuadColors(Lme/jellysquid/mods/sodium/client/model/quad/ModelQuadView;Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/core/BlockPos;Lme/jellysquid/mods/sodium/client/model/light/LightPipeline;Lnet/minecraft/core/Direction;FLme/jellysquid/mods/sodium/client/model/quad/blender/ColorSampler;Lnet/minecraft/world/level/material/FluidState;)V",
 	                at = @At("HEAD"),
 	                argsOnly = true)
 	private final float changeShade(final float brightness) {
