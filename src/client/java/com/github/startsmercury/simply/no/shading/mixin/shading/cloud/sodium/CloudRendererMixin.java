319c6722bc9945923663095b6fb535471e460e5e
StartsMercury <startsmercury@gmail.com>
2023-03-31 21:30:08 +0800

    Should fix #105

--- src/client/java/com/github/startsmercury/simply/no/shading/mixin/shading/cloud/sodium/CloudRendererMixin.java
+++ src/client/java/com/github/startsmercury/simply/no/shading/mixin/shading/cloud/sodium/CloudRendererMixin.java
@@ -1,14 +1,15 @@
 package com.github.startsmercury.simply.no.shading.mixin.shading.cloud.sodium;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import com.github.startsmercury.simply.no.shading.client.Config;
 import com.github.startsmercury.simply.no.shading.client.SimplyNoShading;
 
 import me.jellysquid.mods.sodium.client.render.immediate.CloudRenderer;
-import me.jellysquid.mods.sodium.client.util.color.ColorMixer;
 
 /**
  * The {@code CloudRendererMixin} is a {@linkplain Mixin mixin} class for the
@@ -19,6 +20,12 @@ import me.jellysquid.mods.sodium.client.util.color.ColorMixer;
 @Mixin(CloudRenderer.class)
 public class CloudRendererMixin {
 	/**
+	 * The cached base cloud color.
+	 */
+	@Unique
+	private int baseColor;
+
+	/**
 	 * A private constructor that does nothing as of the writing of this
 	 * documentation.
 	 */
@@ -26,30 +33,45 @@ public class CloudRendererMixin {
 	}
 
 	/**
-	 * This is a {@linkplain Redirect redirector} that redirects all calls to
-	 * {@link ColorMixer#mulARGB(int, int)} in
+	 * This is a {@linkplain ModifyVariable variable modifier} that caches the value
+	 * of the local variable {@code baseColor} in
 	 * {@code CloudRenderer.rebuildGeometry(BufferBuilder, int, int, int)}.
 	 * <p>
-	 * Returns {@code ColorMixer.mulARGB(baseColor, multiplier)} when
-	 * {@linkplain Config#blockShadingEnabled cloud shading is enabled}; otherwise
-	 * the {@code baseColor}
+	 * Returns the parameter {@code baseColor} unmodified.
 	 *
-	 * @param baseColor  the base color
-	 * @param multiplier the multiplier color
-	 * @return {@code ColorMixer.mulARGB(baseColor, multiplier)} when
-	 *         {@linkplain Config#blockShadingEnabled cloud shading is enabled};
-	 *         otherwise the {@code baseColor}
+	 * @param baseColor the base cloud color
+	 * @return the unmodified value of {@code baseColor}
 	 */
-	@Redirect(method = "rebuildGeometry(Lcom/mojang/blaze3d/vertex/BufferBuilder;III)V",
-	          at = @At(value = "INVOKE",
-	                   target = "Lme/jellysquid/mods/sodium/client/util/color/ColorMixer;mulARGB(II)I"),
-	          remap = false)
-	private int modifyMulARGB(final int baseColor, final int multiplier) {
+	@ModifyVariable(method = "rebuildGeometry(Lcom/mojang/blaze3d/vertex/BufferBuilder;III)V",
+	        at = @At(value = "STORE"),
+	        name = "baseColor",
+	        allow = 1,
+	        remap = false)
+	private int cacheBaseColor(final int baseColor) {
+		return this.baseColor = baseColor;
+	}
+
+	/**
+	 * This is an {@linkplain ModifyArg argument modifier} that replaces the
+	 * {@code mixedColor} with the {@code baseColor} in
+	 * {@code CloudRenderer.rebuildGeometry(BufferBuilder, int, int, int)} if
+	 * {@linkplain Config#cloudShadingEnabled cloud shading is disabled}.
+	 *
+	 * @param mixedColor the mixed cloud color
+	 * @return {@code mixedColor} when {@linkplain Config#cloudShadingEnabled cloud
+	 *         shading is enabled}; {@code baseColor} otherwise
+	 */
+	@ModifyArg(method = "rebuildGeometry(Lcom/mojang/blaze3d/vertex/BufferBuilder;III)V",
+	        at = @At(value = "INVOKE",
+	                target = "Lme/jellysquid/mods/sodium/client/render/immediate/CloudRenderer;writeVertex(JFFFI)J"),
+	        index = 4,
+	        remap = false)
+	private int undoColorMixing(final int mixedColor) {
 		final var cloudShadingEnabled = SimplyNoShading.getFirstInstance().getConfig().cloudShadingEnabled;
 
 		if (cloudShadingEnabled)
-			return ColorMixer.mulARGB(baseColor, multiplier);
+			return mixedColor;
 		else
-			return baseColor;
+			return this.baseColor;
 	}
 }
