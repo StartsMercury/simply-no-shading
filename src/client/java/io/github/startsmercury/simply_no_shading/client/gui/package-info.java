cb9e46939413ddff1ada13fa8cecbd25aa3917b2
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-04-06 23:09:02 +0800

    Since the gui and gui/screen

---
+++ src/client/java/io/github/startsmercury/simply_no_shading/client/gui/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Simply No Shading's client graphical user interface subpackage.
+ *
+ * @since 6.2.0
+ */
+package io.github.startsmercury.simply_no_shading.entrypoint;
---
+++ src/client/java/io/github/startsmercury/simply_no_shading/client/gui/screens/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Simply No Shading's client GUI screens subpackage.
+ *
+ * @since 6.2.0
+ */
+package io.github.startsmercury.simply_no_shading.entrypoint;
