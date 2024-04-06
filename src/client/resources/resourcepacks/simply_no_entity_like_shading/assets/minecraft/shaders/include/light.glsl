8595c70ea5f656ec784f5047fc6a52d6d0ef6d70
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-04-06 23:24:46 +0800

    Reword comment

--- src/client/resources/resourcepacks/simply_no_entity_like_shading/assets/minecraft/shaders/include/light.glsl
+++ src/client/resources/resourcepacks/simply_no_entity_like_shading/assets/minecraft/shaders/include/light.glsl
@@ -6,14 +6,13 @@
 /*
  * Entity-like shading is disabled by redifining this very function. Ideally the
  * callers of this function should be modified to distinguish GUI entities and
- * world entities. The limitation of the previous sentence through simple
- * resource pack might be invasive with the overwriting nature and may not work
- * well with other core shaders. A proposed solution is by doing said through
- * transforming the shaders so that way and alike mix-ins would modify calls to
- * this very function while also calculating if it should forego overwriting
- * with disabled shading. It is to note that said solutions are not yet in
- * progress and this long comment is doing many work of documenting and as a
- * TODO for the future.
+ * world entities. The implementation of the previous sentence through simple
+ * resource pack might be invasive with its overwriting nature and may not work
+ * well with other core shaders. A possible solution is by doing said through
+ * transforming the shaders so that way, alike mix-ins, would modify calls to
+ * this very function while also calculating if it should proceed to overwriting
+ * shading. It is to note that said solutions are not yet in progress and this
+ * long comment is doing many work such as documenting and an ellaborate TODO.
  */
 vec4 minecraft_mix_light(vec3 lightDir0, vec3 lightDir1, vec3 normal, vec4 color) {
     return color;
