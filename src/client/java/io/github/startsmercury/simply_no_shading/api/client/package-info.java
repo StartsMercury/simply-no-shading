4b0a2399e8561f9951c9182e35b490fd111ebe86
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-04-16 22:46:43 +0800

    Removed @see tags in api.client package-info

--- src/client/java/io/github/startsmercury/simply_no_shading/api/client/package-info.java
+++ src/client/java/io/github/startsmercury/simply_no_shading/api/client/package-info.java
@@ -4,8 +4,6 @@
  * This package contains client-sided classes and shouldn't be called on
  * non-client environments, specifically the server environment.
  *
- * @see io.github.startsmercury.simply_no_shading.api.client.SimplyNoShading SimplyNoShading
- * @see io.github.startsmercury.simply_no_shading.api.client.Config Config
  * @since 6.2.0
  */
 package io.github.startsmercury.simply_no_shading.api.client;
