0de62e0a2fc42aa35d613bbdb0d6fe81cbf52394
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-04-16 22:32:10 +0800

    Reword javadoc

--- src/client/java/io/github/startsmercury/simply_no_shading/api/client/package-info.java
+++ src/client/java/io/github/startsmercury/simply_no_shading/api/client/package-info.java
@@ -2,7 +2,7 @@
  * Simply No Shading's client API subpackage.
  * <p>
  * This package contains client-sided classes and shouldn't be called on
- * server-only logic.
+ * non-client environments, specifically the server environment.
  *
  * @see io.github.startsmercury.simply_no_shading.api.client.SimplyNoShading SimplyNoShading
  * @see io.github.startsmercury.simply_no_shading.api.client.Config Config
