06a821ea6bfa06dcdbc04e8209fe4bf6e094d79c
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-04-17 00:10:58 +0800

    Move mixin subpackage

---
+++ src/client/java/io/github/startsmercury/simply_no_shading/mixin/client/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Simply No Shading's client mixins.
+ *
+ * @since 6.2.0
+ */
+@ApiStatus.Internal
+package io.github.startsmercury.simply_no_shading.impl.mixin;
+
+import org.jetbrains.annotations.ApiStatus;
