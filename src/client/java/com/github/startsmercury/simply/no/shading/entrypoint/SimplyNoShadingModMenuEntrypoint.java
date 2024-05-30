925676a74d4c1ec27ef9f179ec158de976803fd2
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-05-30 20:52:22 +0800

    Add javadoc tags @since and @deprecated

--- src/client/java/com/github/startsmercury/simply/no/shading/entrypoint/SimplyNoShadingModMenuEntrypoint.java
+++ src/client/java/com/github/startsmercury/simply/no/shading/entrypoint/SimplyNoShadingModMenuEntrypoint.java
@@ -8,8 +8,22 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
  * {@link ModMenuApi} and is an entrypoint defined with the {@code modmenu} key
  * in the {@code fabric.mod.json}. This acts as compatibility between Simply No
  * Shading and ModMenu.
+ *
+ * @since 6.0.0
+ * @deprecated No replacement
  */
 @Deprecated(since = "7.0.0", forRemoval = true)
 @SuppressWarnings({ "all", "removal" })
 public class SimplyNoShadingModMenuEntrypoint implements ModMenuApi {
+    private final ModMenuApi delegate;
+
+    public SimplyNoShadingModMenuEntrypoint() {
+        this.delegate =
+            new io.github.startsmercury.simply_no_shading.impl.client.SimplyNoShadingModMenuEntrypoint();
+    }
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return delegate.getModConfigScreenFactory();
+    }
 }
