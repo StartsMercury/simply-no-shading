51b33a51c97fc6152b3ecae1f237a53a3f0f1b5b
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-06-14 01:20:42 +0800

    Use alternative mixin extra annotation

--- src/client/java/io/github/startsmercury/simply_no_shading/mixin/client/shading/cloud/minecraft/LevelRendererMixin.java
+++ src/client/java/io/github/startsmercury/simply_no_shading/mixin/client/shading/cloud/minecraft/LevelRendererMixin.java
@@ -1,38 +1,27 @@
 package io.github.startsmercury.simply_no_shading.mixin.client.shading.cloud.minecraft;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.startsmercury.simply_no_shading.impl.client.ComputedConfig;
 import net.minecraft.client.renderer.LevelRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LevelRenderer.class)
 public abstract class LevelRendererMixin {
     private LevelRendererMixin() {
     }
 
-    @ModifyConstant(
-        method = """
-            buildClouds(\
-                Lcom/mojang/blaze3d/vertex/BufferBuilder;\
-                D\
-                D\
-                D\
-                Lnet/minecraft/world/phys/Vec3;\
-            )Lcom/mojang/blaze3d/vertex/BufferBuilder$RenderedBuffer;\
-        """,
-        constant = {
-            @Constant(floatValue = 0.9f, ordinal = 0),
-            @Constant(floatValue = 0.9f, ordinal = 1),
-            @Constant(floatValue = 0.9f, ordinal = 2),
-            @Constant(floatValue = 0.7f, ordinal = 0),
-            @Constant(floatValue = 0.7f, ordinal = 1),
-            @Constant(floatValue = 0.7f, ordinal = 2),
-            @Constant(floatValue = 0.8f, ordinal = 0),
-            @Constant(floatValue = 0.8f, ordinal = 1),
-            @Constant(floatValue = 0.8f, ordinal = 2)
-        }
-    )
+    @ModifyExpressionValue(method = "*", at = {
+        @At(value = "CONSTANT", args = "floatValue=0.9", ordinal = 0),
+        @At(value = "CONSTANT", args = "floatValue=0.9", ordinal = 1),
+        @At(value = "CONSTANT", args = "floatValue=0.9", ordinal = 2),
+        @At(value = "CONSTANT", args = "floatValue=0.7", ordinal = 0),
+        @At(value = "CONSTANT", args = "floatValue=0.7", ordinal = 1),
+        @At(value = "CONSTANT", args = "floatValue=0.7", ordinal = 2),
+        @At(value = "CONSTANT", args = "floatValue=0.8", ordinal = 0),
+        @At(value = "CONSTANT", args = "floatValue=0.8", ordinal = 1),
+        @At(value = "CONSTANT", args = "floatValue=0.8", ordinal = 2),
+    })
     private float changeCloudBrightness(final float constantValue) {
         if (ComputedConfig.cloudShadingEnabled)
             return constantValue;
