7cd9cfe1c7fd6b9c6df1bba94a09bfff81abccf4
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-06-14 01:19:15 +0800

    Fix config screen crash

--- src/client/java/io/github/startsmercury/simply_no_shading/impl/client/gui/screens/ConfigScreen.java
+++ src/client/java/io/github/startsmercury/simply_no_shading/impl/client/gui/screens/ConfigScreen.java
@@ -7,6 +7,7 @@ import io.github.startsmercury.simply_no_shading.impl.client.ReloadType;
 import io.github.startsmercury.simply_no_shading.impl.client.ShadingToggle;
 import io.github.startsmercury.simply_no_shading.impl.client.SimplyNoShadingImpl;
 import java.util.Objects;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.OptionInstance;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
@@ -21,7 +22,7 @@ public final class ConfigScreen extends OptionsSubScreen {
     private ReloadType reloadType;
 
     public ConfigScreen(final Screen lastScreen, final Config config) {
-        super(lastScreen, null, ConfigScreen.TITLE);
+        super(lastScreen, Minecraft.getInstance().options, ConfigScreen.TITLE);
 
         Objects.requireNonNull(config, "Parameter config is null");
 
