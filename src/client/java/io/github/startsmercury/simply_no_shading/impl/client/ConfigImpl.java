433672f350f9132ba4cd488d6922495ed058f9a9
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-10-15 21:48:14 +0800

    Iris QoL(?) Compat and internals stuff
    * Extract mod name contained in strings to constant, affected strings are appended/interpolated with said constant
    * Iris support: no world rebuild when editing shading settings with Iris shaders enabled; resource pack reload may still occur though
    * Remove JsonUtils, uses soft-referenced Gson
    * Rename ReloadType to ReloadLevel, level better conveys that one may be bigger than the other
    * Toggle keys are disabled when Iris shaders enabled
    
    (cherry picked from commit 88262377ca3fade8ebfa12070fcbbdf44e11a6b9)

--- src/client/java/io/github/startsmercury/simply_no_shading/impl/client/ConfigImpl.java
+++ src/client/java/io/github/startsmercury/simply_no_shading/impl/client/ConfigImpl.java
@@ -1,91 +1,55 @@
 package io.github.startsmercury.simply_no_shading.impl.client;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import io.github.startsmercury.simply_no_shading.api.client.Config;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
 
 public final class ConfigImpl implements Config {
-    boolean @NotNull [] shadingValues;
+    private boolean blockShadingEnabled;
+    private boolean cloudShadingEnabled;
+    private boolean entityShadingEnabled;
 
-    public ConfigImpl() {
-        this.shadingValues = new boolean[ShadingTarget.valueList().size()];
-    }
+    public ConfigImpl() {}
 
     public ConfigImpl(final Config other) {
-        this();
-        this.set(other);
+        this.setInternal(other);
     }
 
-    public boolean shadingEnabled(final ShadingTarget target) {
-        return this.shadingValues[target.ordinal()];
+    public void set(final Config other) {
+        this.setInternal(other);
     }
 
-    public void setShadingEnabled(final ShadingTarget target, final boolean enabled) {
-        this.shadingValues[target.ordinal()] = enabled;
+    private void setInternal(final Config other) {
+        this.blockShadingEnabled = other.blockShadingEnabled();
+        this.cloudShadingEnabled = other.cloudShadingEnabled();
+        this.entityShadingEnabled = other.entityShadingEnabled();
     }
 
     @Override
     public boolean blockShadingEnabled() {
-        return shadingEnabled(ShadingTarget.BLOCK);
+        return this.blockShadingEnabled;
     }
 
     @Override
     public void setBlockShadingEnabled(final boolean enabled) {
-        setShadingEnabled(ShadingTarget.BLOCK, enabled);
+        this.blockShadingEnabled = enabled;
     }
 
     @Override
     public boolean cloudShadingEnabled() {
-        return shadingEnabled(ShadingTarget.CLOUD);
+        return this.cloudShadingEnabled;
     }
 
     @Override
     public void setCloudShadingEnabled(final boolean enabled) {
-        setShadingEnabled(ShadingTarget.CLOUD, enabled);
+        this.cloudShadingEnabled = enabled;
     }
 
     @Override
     public boolean entityShadingEnabled() {
-        return shadingEnabled(ShadingTarget.ENTITY);
+        return this.entityShadingEnabled;
     }
 
     @Override
     public void setEntityShadingEnabled(final boolean enabled) {
-        setShadingEnabled(ShadingTarget.ENTITY, enabled);
-    }
-
-    public void set(final Config other) {
-        Objects.requireNonNull(other);
-        this.setBlockShadingEnabled(other.blockShadingEnabled());
-        this.setCloudShadingEnabled(other.cloudShadingEnabled());
-        if (other instanceof final ConfigImpl otherImpl) this.setEntityShadingEnabled(otherImpl.entityShadingEnabled());
-    }
-
-    public void fromJson(final JsonElement json) {
-        if (!(json instanceof final JsonObject jsonObject)) {
-            throw new JsonParseException(
-                "Cannot parse "
-                    + this.getClass().getName()
-                    + " from "
-                    + json.getClass().getName()
-            );
-        }
-
-        for (final var target : ShadingTarget.valueList()) {
-            final var enabled = target.fromJson(jsonObject);
-            if (enabled != null) {
-                setShadingEnabled(target, enabled);
-            }
-        }
-    }
-
-    public void toJson(final JsonObject tree) {
-        for (final var target : ShadingTarget.valueList()) {
-            target.intoJson(tree, shadingEnabled(target));
-        }
+        this.entityShadingEnabled = enabled;
     }
 }
