3de96b98c7867b7793f500e86fa02d17f7b7c682
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-04-16 09:22:55 +0800

    Update to Minecraft 25w16a

--- src/client/java/io/github/startsmercury/simply_no_shading/mixin/client/shading/cloud/minecraft/CloudRendererMixin.java
+++ src/client/java/io/github/startsmercury/simply_no_shading/mixin/client/shading/cloud/minecraft/CloudRendererMixin.java
@@ -1,43 +1,26 @@
 package io.github.startsmercury.simply_no_shading.mixin.client.shading.cloud.minecraft;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import io.github.startsmercury.simply_no_shading.impl.client.ComputedConfig;
 import net.minecraft.client.renderer.CloudRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(CloudRenderer.class)
 public abstract class CloudRendererMixin {
     private CloudRendererMixin() {
     }
 
-    @ModifyExpressionValue(
-        method = """
-            buildMesh(                                                          \
-                Lcom/mojang/blaze3d/vertex/Tesselator;                          \
-                I                                                               \
-                I                                                               \
-                Lnet/minecraft/client/CloudStatus;                              \
-                Lnet/minecraft/client/renderer/CloudRenderer$RelativeCameraPos; \
-                Lcom/mojang/blaze3d/pipeline/RenderPipeline;                    \
-            ) Lcom/mojang/blaze3d/vertex/MeshData;                              \
-        """,
-        at = {
-            @At(value = "CONSTANT", args = "floatValue=0.9", ordinal = 0),
-            @At(value = "CONSTANT", args = "floatValue=0.9", ordinal = 1),
-            @At(value = "CONSTANT", args = "floatValue=0.9", ordinal = 2),
-            @At(value = "CONSTANT", args = "floatValue=0.7", ordinal = 0),
-            @At(value = "CONSTANT", args = "floatValue=0.7", ordinal = 1),
-            @At(value = "CONSTANT", args = "floatValue=0.7", ordinal = 2),
-            @At(value = "CONSTANT", args = "floatValue=0.8", ordinal = 5),
-            @At(value = "CONSTANT", args = "floatValue=0.8", ordinal = 6),
-            @At(value = "CONSTANT", args = "floatValue=0.8", ordinal = 7),
-        }
+    @ModifyVariable(
+        method = "encodeFace(Ljava/nio/ByteBuffer;IILnet/minecraft/core/Direction;I)V",
+        at = @At("HEAD"),
+        ordinal = 2,
+        argsOnly = true
     )
-    private float changeCloudBrightness(final float constantValue) {
+    private int changeCloudBrightness(final int flags) {
         if (ComputedConfig.cloudShadingEnabled)
-            return constantValue;
+            return flags;
         else
-            return 1.0f;
+            return flags | CloudRenderer.FLAG_USE_TOP_COLOR;
     }
 }
