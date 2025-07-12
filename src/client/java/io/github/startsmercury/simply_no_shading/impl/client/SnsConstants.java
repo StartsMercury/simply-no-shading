7cf79bfb0dff1eaa62c0dfe965b356c8c8c332fe
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-07-12 20:43:22 +0800

    Moved singleton as field into Minecraft through interface injection
    (cherry picked from commit 62c9775cbaf722731d0407231dd09f9e5a83f608)

---
+++ src/client/java/io/github/startsmercury/simply_no_shading/impl/client/SnsConstants.java
@@ -0,0 +1,13 @@
+package io.github.startsmercury.simply_no_shading.impl.client;
+
+public final class SnsConstants {
+    public static final String MODID = "simply-no-shading";
+
+    public static final String NAME = "Simply No Shading";
+
+    public static final String KEY_CATEGORY = MODID + ".key.categories." + MODID;
+
+    public static final String EXPERIMENTAL_ENTITY_SHADING_ID = "simply_no_entity_like_shading";
+
+    private SnsConstants() {}
+}
