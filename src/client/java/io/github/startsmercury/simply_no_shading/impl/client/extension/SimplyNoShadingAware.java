cfcd1e128456e50e6c8e67861cc89d33a8a18f41
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-07-12 20:43:22 +0800

    Moved singleton as field into Minecraft through interface injection
    (cherry picked from commit 62c9775cbaf722731d0407231dd09f9e5a83f608)

---
+++ src/client/java/io/github/startsmercury/simply_no_shading/impl/client/extension/SimplyNoShadingAware.java
@@ -0,0 +1,7 @@
+package io.github.startsmercury.simply_no_shading.impl.client.extension;
+
+import io.github.startsmercury.simply_no_shading.api.client.SimplyNoShading;
+
+public interface SimplyNoShadingAware {
+    SimplyNoShading getSimplyNoShading();
+}
