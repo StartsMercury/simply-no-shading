1284e2cb8fda77d6a7eb3080d96ca79f00b37f10
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-08-06 19:41:35 +0800

    Naively replace vulnerable imports 3: runtime-only

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,10 +61,15 @@ dependencies {
     loomDevelopmentDependencies(substituteLibs.bundles.loom)
 
     // some minecraft versions require these
-    "minecraftLibraries"(substituteLibs.bundles.minecraft)
-    "minecraftRuntimeLibraries"(substituteLibs.bundles.minecraft)
-    "minecraftClientLibraries"(substituteLibs.bundles.minecraft)
-    "minecraftClientRuntimeLibraries"(substituteLibs.bundles.minecraft)
+    "minecraftLibraries"(substituteLibs.bundles.minecraft.client)
+    "minecraftRuntimeLibraries"(substituteLibs.bundles.minecraft.client)
+    "minecraftRuntimeLibraries"(substituteLibs.minecraft.log4j.core)
+    "minecraftClientLibraries"(substituteLibs.bundles.minecraft.client)
+    "minecraftClientRuntimeLibraries"(substituteLibs.bundles.minecraft.client)
+    "minecraftClientRuntimeLibraries"(substituteLibs.minecraft.log4j.core)
+    "minecraftServerLibraries"(substituteLibs.bundles.minecraft.server)
+    "minecraftServerRuntimeLibraries"(substituteLibs.bundles.minecraft.server)
+    "minecraftServerRuntimeLibraries"(substituteLibs.minecraft.log4j.core)
 }
 
 testing {
