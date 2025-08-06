4c8f7ab8f4ad5d3a37520ad4ecc95ebc4fd79df1
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-08-06 08:38:16 +0800

    Naively replace vulnerable imports from loom and some minecraft versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,3 +12,11 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        val substituteLibs by registering {
+            from(files("gradle/substitute.libs.versions.toml"))
+        }
+    }
+}
