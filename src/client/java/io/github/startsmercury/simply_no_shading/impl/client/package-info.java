9d30f74be21060631d8d7cab8b091e7b1c65735d
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-11-15 13:46:29 +0800

    Update to NullMarked annot (25w34a)

--- src/client/java/io/github/startsmercury/simply_no_shading/impl/client/package-info.java
+++ src/client/java/io/github/startsmercury/simply_no_shading/impl/client/package-info.java
@@ -1,8 +1,4 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@FieldsAreNonnullByDefault
+@NullMarked
 package io.github.startsmercury.simply_no_shading.impl.client;
 
-import javax.annotation.ParametersAreNonnullByDefault;
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
+import org.jspecify.annotations.NullMarked;
