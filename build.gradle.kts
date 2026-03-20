975ae2f20d030ce5b389a4f44163474ad95f577d
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2026-03-20 17:45:10 +0800

    Bump version to 8.0.0-alpha.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 object Constants {
     const val MOD_NAME: String = "Simply No Shading"
-    const val MOD_VERSION: String = "8.0.0-alpha.3"
+    const val MOD_VERSION: String = "8.0.0-alpha.4"
 }
 
 plugins {
